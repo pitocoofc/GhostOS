59a354cd867a763e1a36c33c0f0c6928ff0ae175
Ghost <ismaelkelvyn328@gmail.com>
2026-09-21 10:14:33 -0300

    Update sys_config.js

--- www/sys_config.js
+++ www/sys_config.js
@@ -2,7 +2,7 @@ const GHOST_SYS = {
     cpu: "SnapCobra 695",
     ram: 8,
     ram_virtual: 4,
-    storage_total: 128,
-    os_weight: 35,
+    storage_total: 256,
+    os_weight: 41,
     apps_weight: 7.50
 };
